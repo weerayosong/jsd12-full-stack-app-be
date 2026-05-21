5d7291a2b6ae612a7ef0b6ab7b5febde3e4bbb00
weerayosong <weerayosong@outlook.com>
2026-05-21 13:42:11 +0700

    test auth all done

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import cors from "cors";
 import { connectDB } from "./config/mongodb.js";
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", apiRoutes);
 
